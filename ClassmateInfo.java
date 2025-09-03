cded11d096840b00eec9d55e0872e0c7993a2a4f
Hassan Kdouh <hkdouh@umich.edu>
2025-09-03 18:03:54 -0400

    Add files via upload

---
+++ ClassmateInfo.java
@@ -0,0 +1,16 @@
+public class ClassmateInfo {
+
+        public static void main(String[] args) {
+            String nameAndPronouns = "Motaaz";
+            String major = "Computer Science";
+            String experience = "Couple CIS Courses";
+            String funFact = "I like to travel I recently went to Yosemite";
+            String coolestTech = "AI Medical image scanning to detect cancer";
+
+            System.out.println("Name and preferred pronouns: " + nameAndPronouns);
+            System.out.println("Major: " + major);
+            System.out.println("Previous programming experience: " + experience);
+            System.out.println("Fun fact: " + funFact);
+            System.out.println("Most interesting tech they've seen: " + coolestTech);
+        }
+}
